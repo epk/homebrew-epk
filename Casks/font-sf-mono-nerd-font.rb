f6f1492e366029a43ada19813d615331f162289c
Aditya Sharma <git@adi.run>
2022-11-21 00:54:27 -0800

    Update SF-Mono-Nerd-Font

--- Casks/font-sf-mono-nerd-font.rb
+++ Casks/font-sf-mono-nerd-font.rb
@@ -1,22 +1,22 @@
 cask 'font-sf-mono-nerd-font' do
-  version '15.0d5e1'
-  sha256 '8621fe412854db2846522c65619e635c8968f64b3341910b5286a95c46aac618'
+  version "18.0d1e1"
+  sha256 "1240a6757f1f7b5c0bee61afe185ebf8db65c381bf8ae5cc41956950d85184da"
 
-  url "https://github.com/epk/SF-Mono-Nerd-Font/releases/download/v#{version}/SF-Mono-Nerd-Font.zip"
+  url "https://github.com/epk/SF-Mono-Nerd-Font/archive/v#{version}.zip"
   appcast 'https://github.com/epk/SF-Mono-Nerd-Font/releases.atom'
   name 'SF-Mono-Nerd-Font'
   homepage 'https://github.com/epk/SF-Mono-Nerd-Font'
 
-  font 'SF-Mono-Nerd-Font/SFMono Bold Italic Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Bold Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Heavy Italic Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Heavy Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Light Italic Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Light Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Medium Italic Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Medium Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Regular Italic Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Regular Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Semibold Italic Nerd Font Complete.otf'
-  font 'SF-Mono-Nerd-Font/SFMono Semibold Nerd Font Complete.otf'
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Bold Italic Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Bold Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Heavy Italic Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Heavy Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Light Italic Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Light Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Medium Italic Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Medium Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Regular Italic Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Regular Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Semibold Italic Nerd Font Complete.otf"
+  font "SF-Mono-Nerd-Font-#{version}/SFMono Semibold Nerd Font Complete.otf"
 end
